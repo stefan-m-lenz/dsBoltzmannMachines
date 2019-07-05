6ade451dd545a625aa7d1a585468164174b7a195
Stefan Lenz <lenz@imbi.uni-freiburg.de>
2019-07-05 15:05:22 +0200

    added all fitrbm Arguments (but not all work yet)

--- R/main.R
+++ R/main.R
@@ -6,13 +6,58 @@
 monitored_fitrbmDS <- function(newobj = 'rbm',
                                data = "D",
                                monitoringdata = NULL,
-                               learningrate = NULL) {
+                               nhidden = NULL,
+                               epochs = NULL,
+                               upfactor = NULL,
+                               downfactor = NULL,
+                               learningrate = NULL,
+                               learningrates = NULL,
+                               pcd = NULL,
+                               cdsteps = NULL,
+                               batchsize = NULL,
+                               rbmtype = NULL,
+                               startrbm = NULL) {
 
    x <- as.matrix(eval(parse(text=data)))
+   if (!is.null(epochs)) {
+      epochs <- as.integer(epochs)
+   }
+   if (!is.null(nhidden)) {
+      nhidden <- as.integer(nhidden)
+   }
+   if (!is.null(learningrates)) {
+      learningrates <- as.numeric(unlist(strsplit(learningrates, split=",")))
+   }
+   if (!is.null(cdsteps)) {
+      cdsteps <- as.integer(cdsteps)
+   }
+   if (!is.null(batchsize)) {
+      batchsize <- as.integer(batchsize)
+   }
+   # TODO rbmtype
+   # TODO startrbm
 
-   trainingresult <-
-      monitored_fitrbm(x,
-                       monitoring = juliaExpr("monitorreconstructionerror!"))
+   # Avoid passing NULL arguments to Julia
+   # Collect all the keyword arguments in a list ...
+   kwargs <- list(monitoring = juliaExpr("monitorreconstructionerror!"),
+                  monitoringdata = monitoringdata,
+                  nhidden = nhidden,
+                  epochs = epochs,
+                  upfactor = upfactor,
+                  downfactor = downfactor,
+                  learningrate = learningrate,
+                  learningrates = learningrates,
+                  pcd = pcd,
+                  cdsteps = cdsteps,
+                  batchsize = batchsize,
+                  rbmtype = rbmtype,
+                  startrbm = startrbm)
+
+   # to filter out the null arguments.
+   kwargs <- Filter(Negate(is.null), kwargs)
+   # Then the call can be assembled and evaluated.
+   monitoredfitCall <- as.call(c(list(monitored_fitrbm, x), kwargs))
+   trainingresult <- eval(monitoredfitCall)
 
    monitoringresult <- trainingresult[[1]]
    rbm <- trainingresult[[2]]
