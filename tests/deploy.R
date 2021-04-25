e8f8ae4f71a441653450d4d58e1304cb648cf2ab
Stefan Lenz <lenz@imbi.uni-freiburg.de>
2021-04-25 18:13:36 +0200

    Set target version to 1.0.2

--- tests/deploy.R
+++ tests/deploy.R
@@ -4,4 +4,4 @@ devtools::build()
 o <- opalr::opal.login(username = "administrator",
                        password = "password",
                        url = "http://10.5.10.57:8080")
-opalr::oadmin.install_local_package(o, "../dsBoltzmannMachines_2.0.0.tar.gz")
+opalr::oadmin.install_local_package(o, "../dsBoltzmannMachines_1.0.2.tar.gz")
